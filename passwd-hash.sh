a230b2cec53332e0c71921a783794200a0a62afb
Iurii Ovcharenko <iurii.ovcharenko@dev.zodiac.tv>
2016-02-10 17:37:33 +0200

    cmdline

--- passwd-hash.sh
+++ passwd-hash.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 
 # For busybox MD5
+echo ZoddyY
 openssl passwd -1 ZoddyY
 # For busybox MD5 with my salt
+echo "ZoddyY with salt ABcdEF12"
 openssl passwd -1 -salt ABcdEF12 ZoddyY
+
+# For busybox MD5 from commandline
+if [ -n "$1" ]; then
+  echo "From command line arg[1]"
+  openssl passwd -1 $1
+fi
