0fa325fda09e907ab213408e72d7cca41eb31aba
Iurii Ovcharenko <iurii.ovcharenko@dev.zodiac.tv>
2016-09-26 16:21:57 +0300

    Improve format

--- solution-status-all.sh
+++ solution-status-all.sh
@@ -10,18 +10,20 @@ fi
 for folder in $DIR; do
   pushd "$folder" > /dev/null
     for app in `find . -maxdepth 1 -type d ! -name '.*'`; do
+      repo=0
       type=""
       status=""
       lastrev=""
       lastdate=""
-      printf "=== Status of %-32s " "$app"
+      appmsg=$(printf "=== Status of %-32s " "$app")
       if [ -d $app/.svn ]; then
         pushd $app > /dev/null
           type="SVN"
           status="$(svn status)"
           lastrev="$(svn info | sed -n -e 's/Last Changed Rev: //p')"
           lastdate="$(svn info | sed -n -e 's/Last Changed Date: //p')"
-          popd > /dev/null
+        popd > /dev/null
+        repo=1
       elif [ -d $app/.git ]; then
         pushd $app > /dev/null
           type="GIT"
@@ -29,11 +31,12 @@ for folder in $DIR; do
           lastrev="$(git log -1 --pretty='%h')"
           lastdate="$(git log -1 --pretty='%ad' --date=iso)"
         popd > /dev/null
-      else
-        echo "Unknown CVS type"
+        repo=1
       fi
-      [ -n "$type" ] && if [ "$type" == "SVN" ]; then x="10"; c="revision"; else x="-10"; c="commit  "; fi; printf "%s ${c}: %${x}s Date: %s\n" "$type" "$lastrev" "$lastdate"
-      [ -n "$status" ] && echo "$status"
+      if [ $repo -eq 1 ]; then
+        [ -n "$type" ] && if [ "$type" == "SVN" ]; then x="10"; c="revision"; else x="-10"; c="commit  "; fi; printf "%s %s ${c}: %${x}s Date: %s\n" "$appmsg" "$type" "$lastrev" "$lastdate"
+        [ -n "$status" ] && echo "$status"
+      fi
     done
   popd > /dev/null
 done
