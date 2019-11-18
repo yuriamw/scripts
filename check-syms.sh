e72e20ab938aa3b3113d3fff6448dae510de59b6
Iurii Ovcharenko <iurii.ovcharenko@dev.zodiac.tv>
2019-11-18 14:01:59 +0200

    MINOR: Remove extra line
    Change-Id: I76429a2e9db561d05798f92572eaf9c8bbe84552

--- check-syms.sh
+++ check-syms.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
-SYSROOT=out.charter-arrishydra-powerup-tst/linux/sysroot_stripped
+SYSROOT=$(pwd)/out.charter-arrishydra-powerup-prd/linux/sysroot_stripped
 CONFIG=platforms/arrishydra/check_symbols.yaml
-OUTDIR=./
+OUTDIR=$(pwd)/
 
+rm -rf ${OUTDIR}/{check_symbols_result.txt,dependency_tree.db,deps_table.csv}
 python ./gn/check_missed_symbols.py \
     --sysroot_stripped_path ${SYSROOT} \
     --config_path ${CONFIG} \
     --report_path ${OUTDIR}/check_symbols_result.txt \
     --database_path ${OUTDIR}/dependency_tree.db \
-    --csv_path ${OUTDIR}/deps_table.csv \
+    --csv_path ${OUTDIR}/deps_table.csv
