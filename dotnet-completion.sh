2f1889b2b76556ed2454bdd48a2adce74d325ed1
Iurii Ovcharenko <yuri.amw@gmail.com>
2022-11-02 09:37:02 +0200

    Fix dotnet completion

--- dotnet-completion.sh
+++ dotnet-completion.sh
@@ -1,16 +1,30 @@
 # bash parameter completion for the dotnet CLI
 
-_dotnet_bash_complete()
+# _dotnet_bash_complete()
+# {
+#   local word=${COMP_WORDS[COMP_CWORD]}
+#
+#   local completions
+#   completions="$(dotnet complete --position "${COMP_POINT}" "${COMP_LINE}" 2>/dev/null)"
+#   if [ $? -ne 0 ]; then
+#     completions=""
+#   fi
+#
+#   COMPREPLY=( $(compgen -W "$completions" -- "$word") )
+# }
+#
+# complete -f -F _dotnet_bash_complete dotnet
+
+# bash parameter completion for the dotnet CLI
+
+function _dotnet_bash_complete()
 {
-  local word=${COMP_WORDS[COMP_CWORD]}
+  local cur="${COMP_WORDS[COMP_CWORD]}" IFS=$'\n'
+  local candidates
 
-  local completions
-  completions="$(dotnet complete --position "${COMP_POINT}" "${COMP_LINE}" 2>/dev/null)"
-  if [ $? -ne 0 ]; then
-    completions=""
-  fi
+  read -d '' -ra candidates < <(dotnet complete --position "${COMP_POINT}" "${COMP_LINE}" 2>/dev/null)
 
-  COMPREPLY=( $(compgen -W "$completions" -- "$word") )
+  read -d '' -ra COMPREPLY < <(compgen -W "${candidates[*]:-}" -- "$cur")
 }
 
 complete -f -F _dotnet_bash_complete dotnet
