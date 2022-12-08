8215884735d6dd3ab6662526fef8417d15f34abe
Iurii Ovcharenko <yuri.amw@gmail.com>
2022-12-08 12:32:37 +0200

    Improve go unit test helper script

--- go-unit-test-with-local-mssql.sh
+++ go-unit-test-with-local-mssql.sh
@@ -1,27 +1,30 @@
 #! /bin/bash -e
 
+# --run ^ModelAccessorTestSuite$ -testify.m ^TestModelAccessorAdd$
+
 UNIT_TESTS_MSSQL_DB=${UNIT_TESTS_MSSQL_DB:-"TestSuiteDB"}
 UNIT_TESTS_MSSQL_PASSWD=${UNIT_TESTS_MSSQL_PASSWD:-"ThisIs!Password"}
 
-connection_string=""
-connection_string="${connection_string}azuresqlcreatedb=${UNIT_TESTS_MSSQL_DB};"
-connection_string="${connection_string}Server=tcp:localhost,1433;"
-connection_string="${connection_string}Initial Catalog=master;"
-connection_string="${connection_string}Persist Security Info=False;"
-connection_string="${connection_string}User ID=sa;"
-connection_string="${connection_string}Password=${UNIT_TESTS_MSSQL_PASSWD};"
-connection_string="${connection_string}MultipleActiveResultSets=False;"
-connection_string="${connection_string}TrustServerCertificate=False;"
-connection_string="${connection_string}Connection Timeout=30;"
+connection_string_mssql=""
+connection_string_mssql="${connection_string_mssql}azuresqlcreatedb=${UNIT_TESTS_MSSQL_DB};"
+connection_string_mssql="${connection_string_mssql}Server=tcp:localhost,1433;"
+connection_string_mssql="${connection_string_mssql}Initial Catalog=master;"
+connection_string_mssql="${connection_string_mssql}Persist Security Info=False;"
+connection_string_mssql="${connection_string_mssql}User ID=sa;"
+connection_string_mssql="${connection_string_mssql}Password=${UNIT_TESTS_MSSQL_PASSWD};"
+connection_string_mssql="${connection_string_mssql}MultipleActiveResultSets=False;"
+connection_string_mssql="${connection_string_mssql}TrustServerCertificate=False;"
+connection_string_mssql="${connection_string_mssql}Connection Timeout=30;"
 
-export UNIT_TESTS_MSSQL_CONNECTION_STRING="${connection_string}"
+export UNIT_TESTS_MSSQL_CONNECTION_STRING="${connection_string_mssql}"
+
+connection_string_mongodb="mongodb://localhost:27017/"
+export UNIT_TESTS_COSMOSDB_CONNECTION_STRING=${UNIT_TESTS_COSMOSDB_CONNECTION_STRING:-"$connection_string_mongodb"}
 
 covoutput="$(realpath $PWD/../../../coverage)"
 dir="packages"
 declare -a packages
 packages=()
-
-# --run ^ModelAccessorTestSuite$ -testify.m ^TestModelAccessorAdd$
 
 usage()
 {
@@ -36,6 +39,29 @@ usage()
   echo "        $(basename $0) -p pkg -- -v"
   echo "    is equivalent to"
   echo "        (cd packages/pkg && go test -v)"
+  echo ""
+  echo "    To run specific test use the --run command with regexp pattern for test suite and test case names"
+  echo "    for example"
+  echo "        $(basename $0) -p pkg -- --run ^MyTestSuite$"
+  echo "    is will run all tests in MyTestSuite"
+  echo "        $(basename $0) -p pkg -- --run TestSuite"
+  echo "    is will run all tests in all suites which contains 'MyTestSuite'"
+  echo "    e.g MyTestSuite, FooMyTestSuite, MyTestSuiteBazz, FooMyTestSuiteBar"
+  echo "        $(basename $0) -p pkg -- --run ^MyTestSuite$ -testify.m ^TestItemsAdd$"
+  echo "    is will run single TestItemsAdd test in MyTestSuite"
+  echo "        $(basename $0) -p pkg -- --run ^MyTestSuite$ -testify.m ^TestItems"
+  echo "    is will run all tests which names started with TestItems test in MyTestSuite"
+  echo ""
+  echo "    Environment variables:"
+  echo ""
+  echo "    UNIT_TESTS_MSSQL_DB"
+  echo "        MSSQL database. Default: ${UNIT_TESTS_MSSQL_DB}"
+  echo "    UNIT_TESTS_MSSQL_PASSWD"
+  echo "        MSSQL server password."
+  echo "        Default: ${UNIT_TESTS_MSSQL_PASSWD}"
+  echo "    UNIT_TESTS_COSMOSDB_CONNECTION_STRING"
+  echo "        Cosmos DB connection string."
+  echo "        Default: ${UNIT_TESTS_COSMOSDB_CONNECTION_STRING}"
   echo ""
   echo "    Options:"
   echo "        -h,--help"
