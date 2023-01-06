63324247f06fc92bb403ebe83ab33bc34d84457a
Iurii Ovcharenko <yuri.amw@gmail.com>
2023-01-06 11:22:16 +0200

    Use host data dir for mongo

--- mongo-docker.sh
+++ mongo-docker.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
-docker run -ti --rm -p 27017:27017 mongo
+# -v /my/own/datadir:/data/db -d mongo
+HOST_DATADIR="/home/hdd/mongo/data"
+VOLUME_MOUNT="-v ${HOST_DATADIR}:/data/db"
+
+docker run -ti --rm \
+    -p 27017:27017 \
+    ${VOLUME_MOUNT} \
+    mongo
