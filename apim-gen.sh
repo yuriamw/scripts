44a9a51d026d137833c0923b6a685533975b0bf9
Iurii Ovcharenko <yuri.amw@gmail.com>
2023-08-14 11:50:47 +0300

    az acr example command

--- apim-gen.sh
+++ apim-gen.sh
@@ -2,5 +2,7 @@
 
 tag="${1:-2.1.6}"
 
+# az acr repository show-tags -n secopscommondevacr --repository eplan/apim-template-generator
+
 # docker run -ti --rm -u $UID -v $(pwd):/src secopscommondevacr.azurecr.io/eplan/apim-template-generator:${tag} version
 docker run -ti --rm -u $UID -v $(pwd):/src secopscommondevacr.azurecr.io/eplan/apim-template-generator:${tag} generate -c /src/apim-generator-config.json
