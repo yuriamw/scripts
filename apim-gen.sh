ae4d994db815a0356bf64677d44e84a39b1d20f3
Iurii Ovcharenko <yuri.amw@gmail.com>
2024-03-22 15:58:38 +0200

    Remove apim script

--- apim-gen.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash -x
-
-tag="${1:-3.4.4}"
-
-# az acr repository show-tags -n secopscommondevacr --repository eplan/apim-template-generator
-
-# docker run -ti --rm -u $UID -v $(pwd):/src secopscommondevacr.azurecr.io/eplan/apim-template-generator:${tag} version
-docker run -ti --rm -u $UID -v $(pwd):/src secopscommondevacr.azurecr.io/eplan/apim-template-generator:${tag} generate -c /src/apim-generator-config.json
