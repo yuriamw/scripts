eef0b3cb1e6e4879dbfc0a566fa5655adcf41448
Iurii Ovcharenko <iurii.ovcharenko@zodiacsystems.com>
2024-04-06 12:02:42 +0300

    Aliases for git config

---
+++ bashrc/git-config-users.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# Simple alias to add git config
+function git-config-dtp()
+{
+    git config user.name 'Iurii Ovcharenko'
+    git config user.email 'i.ovcharenko@dtpoland.com'
+}
+
+function git-config-eplan()
+{
+    git config user.name 'Iurii Ovcharenko'
+    git config user.email 'Ovcharenko.I@eplan.de'
+}
+
+function git-config-amw()
+{
+    git config user.name 'Iurii Ovcharenko'
+    git config user.email 'yuri.amw@gmail.com'
+}
