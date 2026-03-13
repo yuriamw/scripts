1960c05163a00bb4c5bc55f03caf17a8b2089a8c
Iurii Ovcharenko <yuri.amw@gmail.com>
2026-03-13 16:19:26 +0200

    Simplify release scripts

--- release-notifier.sh
+++ release-notifier.sh
@@ -14,15 +14,3 @@ echo "golangci/golangci-lint (releases)"
 curl -fsSL 'https://api.github.com/repos/golangci/golangci-lint/releases?per_page=5' \
   | jq -r --arg repo 'golangci/golangci-lint' '.[] | [.published_at, $repo, .tag_name] | @tsv'
 echo
-
-echo "golangci/golangci-lint (tags)"
-curl -fsSL 'https://api.github.com/repos/golangci/golangci-lint/tags?per_page=5' \
-  | jq -r '.[] | [.name, .commit.url] | @tsv' \
-  | while IFS=$'\t' read -r tag commit_url; do
-      printf '%s\t%s\t%s\n' \
-        "$(curl -fsSL "$commit_url" | jq -r '.commit.committer.date')" \
-        'golangci/golangci-lint' \
-        "$tag"
-    done \
-  | sort -r
-echo
