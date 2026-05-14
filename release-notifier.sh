c16424676cab6871f3fd669f1fd61be92cab18c3
Iurii Ovcharenko <yuri.amw@gmail.com>
2026-05-14 10:03:56 +0300

    Update release check

--- release-notifier.sh
+++ release-notifier.sh
@@ -14,3 +14,8 @@ echo "golangci/golangci-lint (releases)"
 curl -fsSL 'https://api.github.com/repos/golangci/golangci-lint/releases?per_page=5' \
   | jq -r --arg repo 'golangci/golangci-lint' '.[] | [.published_at, $repo, .tag_name] | @tsv'
 echo
+
+echo "authzed/spicedb (releases)"
+curl -fsSL 'https://api.github.com/repos/authzed/spicedb/releases?per_page=5' \
+  | jq -r --arg repo 'authzed/spicedb' '.[] | [.published_at, $repo, .tag_name] | @tsv'
+echo
